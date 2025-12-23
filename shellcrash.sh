606b649effb70a231d94ccea8bd87eaf9f7f7695
NasPilot <theaxn@gmail.com>
2025-12-23 16:30:50 +0800

    1.9.3pre3

--- shellcrash.sh
+++ shellcrash.sh
@@ -1,10 +1,13 @@
-#!bin/sh
+#!/bin/sh
 . /etc/profile
+
+# 确保目录存在
+mkdir -p /etc/ShellCrash
 
 # 检测并补全文件
 if [ -z "$(ls -A /etc/ShellCrash)" ] || [ ! -f "/etc/ShellCrash/start.sh" ]; then
-    # 如果文件夹为空，则运行指定命令
-    cp -rL /etc/ShellCrash_bak/* /etc/ShellCrash
+    # 如果文件夹为空或缺少关键文件，从备份中复制
+    cp -rL /etc/ShellCrash_bak/* /etc/ShellCrash/
 fi
 
 # 启动服务
@@ -12,4 +15,4 @@ fi
 echo "ShellCrash启动成功，请进入容器，输入crash进行管理！"
 
 # 保持容器运行
-exec sh
+tail -f /dev/null
